df3cd1805a61f16afd2013f2fafc9ab4e97da7ef
AZR88 <azrielamaldany@gmail.com>
2026-02-22 14:17:04 +0700

    Initial commit

---
+++ src/test/java/helper/utils.java
@@ -0,0 +1,37 @@
+package helper;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import java.time.Duration;
+
+public class utils {
+    private static WebDriver driver;
+    public static WebDriver getDriver(String browser) {
+        if (driver == null) {
+            switch (browser.toLowerCase()) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "edge":
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    break;
+                default:
+                    throw new RuntimeException("Browser tidak didukung: " + browser);
+            }
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            driver.manage().window().maximize();
+        }
+        return driver;
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+}
---
+++ src/test/java/v4/bddpom/steps/hooks.java
@@ -0,0 +1,26 @@
+package v4.bddpom.steps;
+
+import helper.utils;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import org.openqa.selenium.WebDriver;
+
+
+public class hooks {
+    // Kita gunakan static agar driver bisa diakses langsung
+    // oleh Step Definitions tanpa Dependency Injection yang rumit
+    public static WebDriver driver;
+
+    @Before
+    public void setUp() {
+        // Memanggil DriverFactory netral yang sudah kita buat
+        driver = utils.getDriver("chrome");
+        driver.get("https://www.demoblaze.com/");
+    }
+
+    @After
+    public void tearDown() {
+        // Menutup browser setelah skenario selesai
+        utils.quitDriver();
+    }
+}
