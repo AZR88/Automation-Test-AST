1fc1f496e19dad88c2bb598ea044b3b96a4aa0a7
AZR88 <azrielamaldany@gmail.com>
2026-02-23 19:56:14 +0700

    update pom

--- src/test/java/v2/pom/pages/LoginPage.java
+++ src/test/java/v2/pom/pages/LoginPage.java
@@ -1,4 +1,32 @@
 package v2.pom.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class LoginPage {
+    private WebDriver driver;
+
+    // Locator Elemen (Abstraksi Layer)
+    private By loginMenu = By.id("login2");
+    private By usernameField = By.id("loginusername");
+    private By passwordField = By.id("loginpassword");
+    private By loginButton = By.xpath("//button[text()='Log in']");
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // Aksi (Methods)
+    public void clickLoginMenu() {
+        driver.findElement(loginMenu).click();
+    }
+
+    public void enterCredentials(String user, String pass) {
+        driver.findElement(usernameField).sendKeys(user);
+        driver.findElement(passwordField).sendKeys(pass);
+    }
+
+    public void clickLogin() {
+        driver.findElement(loginButton).click();
+    }
 }
--- src/test/java/v2/pom/test/LoginTest.java
+++ src/test/java/v2/pom/test/LoginTest.java
@@ -1,28 +1,35 @@
-package v2.pom.test;
+package v2.pom.tests;
 
 import helper.utils;
 import v2.pom.pages.LoginPage;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class LoginTest {
+    protected WebDriver driver;
+    protected LoginPage login;
 
+    @BeforeMethod
+    public void setUp() {
+        driver = utils.getDriver("chrome");
+        driver.get("https://www.demoblaze.com/");
+        // Inisialisasi Page Object
+        login = new LoginPage(driver);
+    }
 
-        protected WebDriver driver;
-        protected LoginPage loginPage;
+    @Test
+    public void testLoginWithPOM() {
+        login.clickLoginMenu();
+        login.enterCredentials("azriel_test", "password123");
+        login.clickLogin();
 
-        @BeforeMethod
-        public void setUp() {
-            driver = utils.getDriver("chrome");
-            driver.get("https://www.demoblaze.com/");
-            // POM: Initialize page object with driver
-            loginPage = new LoginPage(driver);
-        }
+        System.out.println("Skenario POM Login Selesai.");
+    }
 
-        @AfterMethod
-        public void tearDown() {
-            utils.quitDriver();
-        }
+    @AfterMethod
+    public void tearDown() {
+        utils.quitDriver();
     }
 }
