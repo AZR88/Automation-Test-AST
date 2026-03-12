8e253fbd8063981caf23396ac34a009a4a9523a1
AZR88 <azrielamaldany@gmail.com>
2026-03-12 21:44:14 +0700

    update v4 product

---
+++ src/test/java/v4/bddpom/page/ProductPage.java
@@ -0,0 +1,89 @@
+package v4.bddpom.page;
+
+import helper.WaitElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class ProductPage {
+
+    public static By productPic = By.xpath("//div[@id='imgp']//img");
+    public static By productTitle = By.xpath("//h2[@class='name']");
+    public static By productPrice = By.xpath("//div[@id='tbodyid']/h3[@class='price-container']");
+    public static By addToCart = By.xpath("//a[@class='btn btn-success btn-lg' and text()='Add to cart']");
+
+    public static boolean isProductDescriptionDisplayed(WebDriver driver, String productdesc) {
+        By descLocator = By.xpath("//p[contains(text(),'" + productdesc + "')]");
+        try {
+            WaitElement.waitForElement(descLocator);
+            WebElement productDescriptionElement = driver.findElement(descLocator);
+            return productDescriptionElement.isDisplayed();
+        } catch (TimeoutException | NoSuchElementException e) {
+            System.out.println("Element with product description '" + productdesc + "' not found or not visible.");
+            return false;
+        }
+    }
+
+
+    public static String  checkTitle(WebDriver driver) {
+        WaitElement.waitForElement(productTitle);
+        WebElement itemTitle = driver.findElement(productTitle);
+        return itemTitle.getText();
+    }
+
+    public static String checkPrice(WebDriver driver) {
+        WaitElement.waitForElement(productPrice);
+        WebElement itemPrice = driver.findElement(productPrice);
+        String text = itemPrice.getText();
+        String actualPrice = text.split("\n")[0].replaceAll("[^\\d$.]", "").trim();
+        return  actualPrice;
+    }
+
+
+
+    public static boolean checkPic(WebDriver driver){
+        try {
+            WaitElement.waitForElement(productPic);
+            WebElement itemPic = driver.findElement(productPic);
+            return itemPic.isDisplayed();
+        } catch ( TimeoutException | NoSuchElementException e){
+            return false;
+        }
+    }
+
+    public static Boolean clickAdd (WebDriver driver){
+                try {
+                    WaitElement.waitForElement(addToCart);
+                    WebElement add = driver.findElement(addToCart);
+                    add.click();
+                    return true;
+                }catch (TimeoutException | NoSuchElementException e){
+                    return false;
+                }
+            }
+
+
+
+    public static String getAlertText(WebDriver driver) {
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+            String alertText = alert.getText();
+            alert.accept();
+            return alertText;
+        } catch (TimeoutException e) {
+            System.out.println("No alert detected within the timeout");
+            return null;
+        }
+    }
+
+}
+
+
+
+
+
+
+
---
+++ src/test/java/v4/bddpom/steps/ProductStepDef.java
@@ -0,0 +1,64 @@
+package v4.bddpom.steps;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import v4.bddpom.page.Cart;
+import v4.bddpom.page.Item;
+import v4.bddpom.page.ProductPage;
+
+import static helper.WebHelper.driver;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+public class ProductStepDef {
+    @When("the user click the product title {string}")
+    public void theUserClickTheProductTitle(String title) {
+        boolean select = Item.selectItemByName(driver, title);
+        assertTrue("Item not selected",select);
+    }
+
+    @Then("the product title should be {string}")
+    public void theProductTitleShouldBe(String title) {
+        String text =  ProductPage.checkTitle(driver);
+        assertEquals("The tilte is not match, expected ="+title+"",text, title);
+    }
+
+    @Then("the product price should be {string}")
+    public void theProductPriceShouldBe(String price) {
+        String text = ProductPage.checkPrice(driver);
+        assertEquals("Price mismatch! Expected: " + price + ", but Actual: " + text, price, text);
+
+    }
+
+    @Then("the product image should be displayed")
+    public void theProductImageShouldBeDisplayed() {
+        boolean isDisplayed = ProductPage.checkPic(driver);
+        assertTrue("picture is not Displayed", isDisplayed);
+    }
+    @Then("the product description should contain {string}")
+    public void theProductDescriptionShouldContain(String productdesc) {
+        boolean isDisplayed = ProductPage.isProductDescriptionDisplayed(driver, productdesc);
+        assertTrue("Product with desc '" + productdesc + "' Not seen in this page!", isDisplayed);
+    }
+
+
+
+    @Then("the product {string} should be added to the cart successfully")
+    public void theProductShouldBeAddedToTheCartSuccessfully(String title) {
+       String text = Cart.checkTitle(driver);
+       assertEquals(title,text);
+    }
+
+
+    @Then("the user clicks the Add to cart button")
+    public void theUserClicksTheAddToCartButton() {
+        boolean button = ProductPage.clickAdd(driver);
+       assertTrue("button not displayed",button);
+    }
+
+
+    @Then("Show Alert message {string}")
+    public void showAlertMessage(String massage) {
+        String text = ProductPage.getAlertText(driver);
+        assertEquals(text,massage);
+    }
+}
