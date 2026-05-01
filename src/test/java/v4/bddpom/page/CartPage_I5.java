0e7ab58f2baa915f5d06d7b85ab3346525050ae8
AZR88 <azrielamaldany@gmail.com>
2026-05-01 22:37:45 +0700

    Add Change Injection V4 Page

---
+++ src/test/java/v4/bddpom/page/CartPage_I5.java
@@ -0,0 +1,88 @@
+package v4.bddpom.page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.util.List;
+
+import static helper.WebHelper.driver;
+
+public class CartPage_I5 extends BasePage {
+
+    private static final By CART_ITEM = By.xpath("//tbody[@id='tbodyid']//tr[@class='success']/td[2]");
+    private static final By DELETE_ITEM = By.xpath("//tbody[@id='tbodyid']//tr[@class='success']//a[contains(text(),'Delete')]");
+    private static final By TOTAL_PRICE = By.id("totalp");
+    private static final By ORDER_BUTTON = By.xpath("//button[text()='Place Order']");
+    private static final By PURCHASE_BUTTON = By.xpath("//button[text()='Purchase']");
+    private static final By PROMO_INPUT = By.id("promo");
+    private static final By ALL_PRICE = By.xpath("//tbody[@id='tbodyid']//tr/td[3]");
+    private static final By CONFIRM_PURCHASE = By.xpath("//div[@class='sa-confirm-button-container']");
+
+    public void clickOrder() {
+        click(ORDER_BUTTON);
+    }
+
+    public void clickPurchase() {
+        click(PURCHASE_BUTTON);
+    }
+
+    public void inputPromo(String promoCode) {
+        sendKeys(PROMO_INPUT, promoCode);
+    }
+
+    public void clickConfirmPurchase() {
+        click(CONFIRM_PURCHASE);
+    }
+
+    public void deleteItem() {
+        click(DELETE_ITEM);
+    }
+
+    public String checkTitle() {
+        return getText(CART_ITEM);
+    }
+
+    public String checkPrice() {
+        return getText(ALL_PRICE);
+    }
+
+    public String getTotalPrice() {
+        return getText(TOTAL_PRICE);
+    }
+
+    public void fillField(String fieldId, String value) {
+        By locator = By.id(fieldId);
+        sendKeys(locator, value);
+    }
+
+    public boolean isItemRemoved(String itemName) {
+        By itemLocator = By.xpath("//tbody[@id='tbodyid']//tr[td[2][text()='" + itemName + "']]");
+        try {
+            return wait.until(ExpectedConditions.invisibilityOfElementLocated(itemLocator));
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public int[] calculateTotalPrice() {
+        List<WebElement> prices = driver.findElements(ALL_PRICE);
+        int calculated = 0;
+
+        for (WebElement el : prices) {
+            String text = el.getText().trim().replaceAll("[^0-9]", "");
+            if (!text.isEmpty()) calculated += Integer.parseInt(text);
+        }
+
+        int displayed = Integer.parseInt(getTotalPrice().replaceAll("[^0-9]", ""));
+        return new int[]{calculated, displayed};
+    }
+
+    public String getAlertText() {
+        return super.getAlertText();
+    }
+
+
+    public void acceptAlert() {
+        super.acceptAlert();
+    }
+}
