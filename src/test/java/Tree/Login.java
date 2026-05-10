2778fd68deca8f5c9285116e4c464bc4f26483ab
AZR88 <azrielamaldany@gmail.com>
2026-05-11 00:25:12 +0700

    add tree ast

---
+++ src/test/java/Tree/Login.java
@@ -0,0 +1,7 @@
+package Tree;
+
+import org.openqa.selenium.By;
+
+public class Login {
+    private static final By USERNAME_INPUT = By.cssSelector("#loginusername");
+}
---
+++ src/test/java/Tree/Snipet_V1.java
@@ -0,0 +1,7 @@
+package Tree;
+
+import org.openqa.selenium.By;
+
+public class Snipet_V1 {
+    private static final By USERNAME_INPUT = By.cssSelector("#user_v2");
+}
